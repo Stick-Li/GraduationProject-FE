e7578c1c8b3d7ea713c11364ab096e3977c5ecc7
lipeiying <lipeiying02@corp.netease.com>
2023-04-28 14:08:34 +0800

    保存

--- src/pages/TestRoute/index.jsx
+++ src/pages/TestRoute/index.jsx
@@ -1,28 +1,66 @@
-import { Button, Modal } from 'antd';
-import React, { useState } from 'react';
-const App = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const showModal = () => {
-    setIsModalOpen(true);
-    console.log(11111)
-  };
-  const handleOk = () => {
-    setIsModalOpen(false);
-  };
-  const handleCancel = () => {
-    setIsModalOpen(false);
-  };
+import { Button } from 'antd';
+import React, { useContext, createContext, useState, useEffect } from 'react';
+
+const TestContext = createContext();
+
+const Navbar = () => {
+  const { username } = useContext(TestContext)
+
   return (
-    <>
-      <Button type="primary" onClick={showModal}>
-        Open Modal
-      </Button>
-      <Modal title="Basic Modal" visible={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-      </Modal>
-    </>
-  );
-};
-export default App;
+    <div className="navbar">
+      <p>Navbar:{username}</p>
+      <Messages />
+    </div>
+  )
+}
+
+const Messages = () => {
+  const { username } = useContext(TestContext)
+  console.log('触发渲染', username)
+  useEffect(() => {
+    console.log('触发执行')
+  }, [username]);
+  return (
+    <div className="messages">
+      <p>1 message for :{username}</p>
+    </div>
+  )
+}
+
+
+const Index = () => {
+
+  // if (Math.random() > 0.5) {
+  //   useState(10000) // eslint-disable-line no-unused-vars
+  // }
+  // if (Math.random() > 0.5) {
+  //   useState(10000, 'key1');
+  // }
+
+  const [value, setValue] = useState(0);
+
+  const [useName, setUseName] = useState('superawesome');
+
+
+
+  return (
+    // <>
+    //   <TestContext.Provider
+    //     value={{
+    //       username: useName,
+    //     }}
+    //   >
+    //     <div className="test">
+    //       <Navbar />
+    //       <Button onClick={() => setUseName('🍩')}>点击改变context</Button>
+    //     </div>
+    //   </TestContext.Provider>
+    // </>
+    <div>
+      <button onClick={() => setValue(value + 1)}>+</button>
+      {value}
+    </div>
+  )
+}
+
+export default Index;
